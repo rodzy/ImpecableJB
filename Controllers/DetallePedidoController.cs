e111647b992f6706029032ccc3b243be3010f3e1
rodzy <49137701+rodzy@users.noreply.github.com>
2020-04-08 23:55:37 -0600

    untracked files on master: 28e67fd Registro de usuario completo, registro de cupones en desarrollo, carrito completado, un poco de diseño CSS agregado

---
+++ Controllers/DetallePedidoController.cs
@@ -0,0 +1,22 @@
+﻿using ImpecableJB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImpecableJB.Controllers
+{
+    /// <summary>
+    /// La clase Detalle pedido que guarda todos los pedidos antes
+    /// </summary>
+    public class DetallePedidoController : Controller
+    {
+        ImpecableContext db = new ImpecableContext();
+        // GET: DetallePedido
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
